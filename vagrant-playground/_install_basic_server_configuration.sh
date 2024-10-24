3b8c063df79dc7fdb04c65e64b6c4e02c8c573e4
stephane-klein <contact@stephane-klein.info>
2024-10-24 23:43:38 +0200

    Execute apt upgrade -y

--- vagrant-playground/_install_basic_server_configuration.sh
+++ vagrant-playground/_install_basic_server_configuration.sh
@@ -5,7 +5,7 @@ set -evuo pipefail
 export DEBIAN_FRONTEND=noninteractive
 echo '* libraries/restart-without-asking boolean true' | sudo debconf-set-selections
 apt-get update -y
-#apt-get upgrade -y
+apt-get upgrade -y
 apt-get install -yq \
     apt-transport-https \
     ca-certificates \
